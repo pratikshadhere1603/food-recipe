55d5edbf6a2b591474c031191024c8423b895f97
pratikshadhere1603 <pratikshadhere930@gmail.com>
2022-10-08 00:18:14 +0530

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', (e) => {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+})
+
+function showOne(id) {
+    $('#' + id).show();
+    $('.hide').not('#' + id).hide();
+}
